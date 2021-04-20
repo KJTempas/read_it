a4de39bd078cfce7c9271156b1b6688ebf844565
KJTempas <kjtempas@gmail.com>
2021-04-19 19:30:34 -0500

    add new book form

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,4 +6,14 @@ class BooksController < ApplicationController
     def index
         @books = Book.all
     end
+
+    def new
+    end
+
+    def create
+        @book = Book.new(params.require(:book).permit(:title, :author))
+        @book.save
+        redirect_to @book
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   root 'pages#home'
   get 'about', to: 'pages#about'
   #resources :books #gives all routes for books 
-  resources :books, only: [:show, :index]
+  resources :books, only: [:show, :index, :new, :create]
 end

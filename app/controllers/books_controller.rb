632f49e351e1e7b72a32861e76247c03efa9ba37
KJTempas <kjtempas@gmail.com>
2021-04-20 10:31:31 -0500

    add CRUD operations

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,12 +8,37 @@ class BooksController < ApplicationController
     end
 
     def new
+        @book = Book.new
+    end
+
+    def edit
+        @book = Book.find(params[:id]) #find the article
     end
 
     def create
         @book = Book.new(params.require(:book).permit(:title, :author))
-        @book.save
-        redirect_to @book
+        if @book.save
+            flash[:notice] = "Book was added successfully"
+            redirect_to @book
+        else
+            render 'new'
+        end
     end
+
+    def update
+        @book = Book.find(params[:id])
+        if @book.update(params.require(:book).permit(:title, :author))
+            flash[:notice] = "Book was updated successfully."
+            redirect_to @book 
+        else
+            render 'edit'
+        end
+    end
+
+def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
+end
 
 end

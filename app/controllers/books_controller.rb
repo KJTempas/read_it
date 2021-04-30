39ed0703613debcdfbae126166fd0608e5c6b97f
KJTempas <kjtempas@gmail.com>
2021-04-30 17:44:48 -0500

    add pagination to books list

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,7 +6,8 @@ class BooksController < ApplicationController
     end
 
     def index
-        @books = Book.all
+        #@books = Book.all
+        @books = Book.paginate(page: params[:page], per_page: 5)
     end
 
     def new

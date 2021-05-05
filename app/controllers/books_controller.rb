94daa7d468b78a138364f7129a144cf2bc51fdcd
KJTempas <kjtempas@gmail.com>
2021-05-05 15:55:41 -0500

    add helper method for auth in controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -19,7 +19,7 @@ class BooksController < ApplicationController
 
     def create
         @book = Book.new(book_params)
-        @book.user = User.first
+        @book.user = current_user
         if @book.save
             flash[:notice] = "Book was added successfully"
             redirect_to @book
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
+    before_action :set_user, only: [:show, :edit, :update]
+
     def show
-        @user = User.find(params[:id])
         #@books = @user.books #articles belonging to this user instance
         @books = @user.books.paginate(page: params[:page], per_page: 4)
     end
@@ -15,11 +16,9 @@ class UsersController < ApplicationController
     end
 
     def edit
-        @user = User.find(params[:id])
     end
 
     def update
-        @user = User.find(params[:id])
         if @user.update(user_params) #if updated is successful, send message to user
             flash[:notice] = "Your account information was successfully updated"
             redirect_to @user
@@ -31,6 +30,7 @@ class UsersController < ApplicationController
     def create
         @user = User.new(user_params)
         if @user.save
+            session[:user_id] = @user.id
             flash[:notice] = "Welcome to Read It! #{@user.username}- you have successfully signed up"
             redirect_to books_path
         else   
@@ -42,4 +42,9 @@ class UsersController < ApplicationController
     def user_params
         params.require(:user).permit(:username, :email, :password)
     end
+
+    def set_user
+        @user = User.find(params[:id])
+    end
+
 end
